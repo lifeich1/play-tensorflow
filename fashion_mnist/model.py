b5396f0e3a1d2405db546570c2d6a50e7b65811f
lifeich1 <lifeich0@gmail.com>
2018-10-16 11:26:47 +0800

    :sparkles: CNN model like ina-speech-music

--- fashion_mnist/model.py
+++ fashion_mnist/model.py
@@ -1,11 +1,34 @@
 import tensorflow as tf
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28, )),
-    tf.keras.layers.Dense(128, activation=tf.nn.relu),
-    tf.keras.layers.Dropout(0.4),
-    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
-])
+model = tf.keras.Sequential()
+
+model.add(tf.keras.layers.Reshape((28, 28, 1), input_shape=(28, 28, )))
+model.add(tf.keras.layers.Conv2D(32, (5, 5)))
+model.add(tf.keras.layers.BatchNormalization(3))
+model.add(tf.keras.layers.Activation(tf.nn.relu))
+model.add(tf.keras.layers.MaxPool2D((2, 2, )))
+model.add(tf.keras.layers.Conv2D(64, (5, 5)))
+model.add(tf.keras.layers.BatchNormalization(3))
+model.add(tf.keras.layers.Activation(tf.nn.relu))
+model.add(tf.keras.layers.MaxPool2D((2, 2, )))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(1024))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.Activation(tf.nn.relu))
+model.add(tf.keras.layers.Dropout(0.2))
+model.add(tf.keras.layers.Dense(512))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.Activation(tf.nn.relu))
+model.add(tf.keras.layers.Dropout(0.3))
+model.add(tf.keras.layers.Dense(512))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.Activation(tf.nn.relu))
+model.add(tf.keras.layers.Dropout(0.4))
+model.add(tf.keras.layers.Dense(512))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.Activation(tf.nn.relu))
+model.add(tf.keras.layers.Dropout(0.5))
+model.add(tf.keras.layers.Dense(10, activation=tf.nn.softmax))
 
 model.compile(optimizer=tf.train.AdamOptimizer(),
               loss='sparse_categorical_crossentropy',
